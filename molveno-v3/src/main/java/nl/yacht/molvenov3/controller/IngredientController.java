7034e9268f9f224abfb300572693e11f35229d3a
BasvBo <37073686+BasvBo@users.noreply.github.com>
2018-03-20 16:31:32 +0100

    first get en save

--- molveno-v3/src/main/java/nl/yacht/molvenov3/controller/IngredientController.java
+++ molveno-v3/src/main/java/nl/yacht/molvenov3/controller/IngredientController.java
@@ -1,4 +1,29 @@
 package nl.yacht.molvenov3.controller;
 
+import nl.yacht.molvenov3.model.Ingredient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class IngredientController {
+
+    @GetMapping("/api/getIngrient")
+    public Ingredient getIngrient(){
+
+        Ingredient result = new Ingredient("pasta", true, 100, 3.40);
+
+        return result;
+    }
+
+    @PostMapping
+    public void save(@RequestBody Ingredient ingredient){
+        System.err.println(ingredient.isAllergen());
+        System.err.println(ingredient.getNumberOfStock());
+        System.err.println(ingredient.getCostPrice());
+    }
+
+
+
 }
--- molveno-v3/src/main/java/nl/yacht/molvenov3/model/Ingredient.java
+++ molveno-v3/src/main/java/nl/yacht/molvenov3/model/Ingredient.java
@@ -1,4 +1,52 @@
 package nl.yacht.molvenov3.model;
 
 public class Ingredient {
+
+    private String name;
+    private boolean isAllergen;
+    //private Distributor distributor;
+    private int numberOfStock;
+    private double costPrice;
+
+
+    public Ingredient(){}
+
+    public Ingredient(String name, boolean isAllergen, int numberOfStock, double costPrice) {
+        this.name = name;
+        this.isAllergen = isAllergen;
+        this.numberOfStock = numberOfStock;
+        this.costPrice = costPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isAllergen() {
+        return isAllergen;
+    }
+
+    public void setAllergen(boolean allergen) {
+        isAllergen = allergen;
+    }
+
+    public int getNumberOfStock() {
+        return numberOfStock;
+    }
+
+    public void setNumberOfStock(int numberOfStock) {
+        this.numberOfStock = numberOfStock;
+    }
+
+    public double getCostPrice() {
+        return costPrice;
+    }
+
+    public void setCostPrice(double costPrice) {
+        this.costPrice = costPrice;
+    }
 }
