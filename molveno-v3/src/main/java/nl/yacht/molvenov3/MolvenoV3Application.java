484198c8c28f8735cdc1f4514275d874c5ae501d
Mengerjm <mengerjm@gmail.com>
2018-03-20 14:32:19 +0100

    New
    new

---
+++ molveno-v3/src/main/java/nl/yacht/molvenov3/MolvenoV3Application.java
@@ -0,0 +1,12 @@
+package nl.yacht.molvenov3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolvenoV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolvenoV3Application.class, args);
+	}
+}
