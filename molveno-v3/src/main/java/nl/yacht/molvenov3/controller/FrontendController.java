1a2dd8d03ff734bde601f373a534a95403cc2c3a
Natasja Eibrink <natasjaeibrink@gmail.com>
2018-03-28 13:41:06 +0200

    xx
    xx

--- molveno-v3/src/main/java/nl/yacht/molvenov3/controller/FrontendController.java
+++ molveno-v3/src/main/java/nl/yacht/molvenov3/controller/FrontendController.java
@@ -3,6 +3,7 @@ package nl.yacht.molvenov3.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Map;
 
@@ -10,7 +11,7 @@ import java.util.Map;
 @RequestMapping(value="/")
 public class FrontendController {
 
-    @GetMapping
+    @RequestMapping(value="", method= RequestMethod.GET)
     public String home(Map<String, Object> model){
         return "index";
     }
