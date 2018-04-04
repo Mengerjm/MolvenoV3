01070c0f9d43edcbaa89e0183c2c3ff44b08f444
Mengerjm <mengerjm@gmail.com>
2018-04-04 14:59:23 +0200

    Datetime time
    Updated

--- molveno-v3/src/main/java/nl/yacht/molvenov3/model/Reservation.java
+++ molveno-v3/src/main/java/nl/yacht/molvenov3/model/Reservation.java
@@ -15,7 +15,7 @@ public class Reservation implements Serializable {
     private String telephoneNumber;
     private String email;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime reservationTime;
     private long id;
     private List<Table> reservedTable;
