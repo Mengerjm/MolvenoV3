e00453c6014ee86f42df7e9a90d3fdb4856ecc4b
Petri Stegeman <petri_sl@hotmail.com>
2018-04-05 15:09:05 +0200

    Table fixed
    IS fixed?

--- molveno-v3/src/main/java/nl/yacht/molvenov3/controller/TableController.java
+++ molveno-v3/src/main/java/nl/yacht/molvenov3/controller/TableController.java
@@ -4,8 +4,13 @@ import nl.yacht.molvenov3.model.Table;
 import nl.yacht.molvenov3.repository.CrudTableRepository;
 import nl.yacht.molvenov3.repository.ReservationRepository;
 import nl.yacht.molvenov3.repository.TableRepository;
+import nl.yacht.molvenov3.util.TableUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/table")
@@ -17,13 +22,14 @@ public class TableController {
     @Autowired
     private CrudTableRepository tableRepository;
     //private ReservationRepository reservationRepository;
-/*
+
     //Show all available tables
     @GetMapping(value = "/available")
     public Iterable<Table> findAllAvailable() {
-        return this.tables.findAllAvailable();
+        Iterable<Table> allTables = this.crudTableRepository.findAll();
+        return TableUtil.findAllAvailable(allTables);
     }
-*/
+
     //Show all tables
     @GetMapping(value = "/findall")
     public Iterable<Table> findAll() {
@@ -44,16 +50,17 @@ public class TableController {
 
     //Change table characteristics
     @PutMapping(value = "/update/{id}")
-    public Table update(@PathVariable("id") int tableNumber, @RequestBody Table input) {
-        return this.crudTableRepository.save( input);
+    public Table update(@PathVariable("id") Long tableNumber, @RequestBody Table input) {
+        return this.crudTableRepository.save(input);
     }
-/*
+
     //Set table unavailable now for random walk in guests
     @PutMapping(value = "/available/{id}")
-    public Table setUnavailable(@PathVariable("id") int tableNumber) {
-        return this.tables.setUnavailable(tableNumber);
+    public Table setUnavailable(@PathVariable("id") Long tableNumber) {
+        Table newTable = this.crudTableRepository.findOne(tableNumber);
+        return save(newTable);
     }
-*/
+
     //Delete a table by tablenumber
     @DeleteMapping(value = "{id}")
     public void delete(@PathVariable("id") Long tableNumber) {
--- molveno-v3/src/main/java/nl/yacht/molvenov3/util/TableUtil.java
+++ molveno-v3/src/main/java/nl/yacht/molvenov3/util/TableUtil.java
@@ -1,5 +1,26 @@
 package nl.yacht.molvenov3.util;
 
+import nl.yacht.molvenov3.model.Table;
+import nl.yacht.molvenov3.repository.CrudTableRepository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TableUtil {
-    //Kind of empty
+
+    public static Iterable<Table> findAllAvailable(Iterable<Table> allTables){
+        List<Table> availableTables = new ArrayList<>();
+        for (Table table: allTables) {
+            if(table.canTableBeUsedNow()){
+                availableTables.add(table);
+            }
+        }
+        return availableTables;
+    }
+
+    public static Table setUnavailable(Table table) {
+        table.getReservationTimes().add(LocalDateTime.now());
+        return table;
+    }
 }
